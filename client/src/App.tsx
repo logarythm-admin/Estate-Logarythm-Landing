1ed248de3b0f3b478e38c6c40c09d52f1b0bfb9d
Hanoon02 <ahmedhanoon02@gmail.com>
2026-02-19 22:32:56 +0530

    Build Fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,8 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import Landing from "@/pages/landing";
+import NotFound from "./pages/not-found";
+import Landing from "./pages/landing";
 
 function Router() {
   return (
